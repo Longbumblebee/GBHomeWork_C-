bc836c6f2985b33a3ffaa611660e9666064ce037
longbumblebee <kz-vlad@mail.ru>
2022-08-30 22:58:59 +0300

    Homework 2.1

---
+++ BankAccount/BankAccount/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+
+
+namespace BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            Account account = new Account();
+
+            account.FillTypeAccount(TypeOfBankAccount.DEBIT);
+
+            account.FillNumber(random.Next(1, 99999999));
+
+            account.FillBalance(100000);
+
+            account.FullInfo();
+        }
+    }
+}
